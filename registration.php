02fb7f7cbd1ec8a6020a0ccfda4b0df9e564d3c1
Dev <vquangluong@gmail.com>
2023-06-22 16:41:48 +0700

    init source

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'EBoost_DisableCompareProducts',
+    __DIR__
+);
